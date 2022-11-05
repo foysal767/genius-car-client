aeb2507f8fd74dcc5e0ba40111eb035a3746707e
foysal767 <uddinmohammadfoysal981@gmail.com>
2022-11-05 11:23:46 +0600

    order add, cancel added

--- src/contexts/AuthProvider/AuthProvider.js
+++ src/contexts/AuthProvider/AuthProvider.js
@@ -8,9 +8,11 @@ const AuthProvider = ({children}) => {
     const [user, setUser] = useState(null)
     const [loading, setLoading] = useState(true)
     const createUser = (email, password) =>{
+        setLoading(true)
         return createUserWithEmailAndPassword(auth, email, password);
     }
     const login = (email, password) =>{
+        setLoading(true)
         return signInWithEmailAndPassword(auth, email, password);
     }
     const authInfo = {
@@ -23,6 +25,7 @@ const AuthProvider = ({children}) => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
             console.log(currentUser)
             setUser(currentUser)
+            setLoading(false)
         })
         return unsubscribe()
     }, [])
